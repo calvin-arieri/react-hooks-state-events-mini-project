a94625d09f0743c7695a0f3a8bf0cc9a73e4127c
calvin-arieri <calvin.arieri@student.moringaschoool.com>
2023-05-05 17:29:58 +0300

    done with assignment

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,9 +1,16 @@
 import React from "react";
 
-function TaskList() {
+function TaskList({passing_task}) {
   return (
     <div className="tasks">
       {/* display a list of tasks using Task component */}
+      <ul>
+        {passing_task.map((task) =>{
+          return(
+            <li key={task.text}>{task.text} {task.category}</li>
+          )
+        })}
+      </ul>
     </div>
   );
 }
